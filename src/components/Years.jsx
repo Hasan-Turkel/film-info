23b8e6ec4be4db5431eff45414b0cc286562bcda
hasan <mmhotmail>
2023-07-25 21:44:21 +0300

    eslinti fixed commit

--- src/components/Years.jsx
+++ src/components/Years.jsx
@@ -3,8 +3,9 @@
 const Years = ({data, index, setIndex}) => {
   return (
     <section className="d-flex gap-4 m-3">
-    {data?.map(({date}, i)=><h3 onClick={()=>setIndex(i)} role="button" className={i==index?"text-danger border-bottom border-danger border-5 bg-secondary-subtle w-25 m-auto" :"text-secondary bg-secondary-subtle w-25 m-auto"}
+    {data?.map(({date}, i)=><h3 onClick={()=>setIndex(i)} role="button" className={i===index?"text-danger border-bottom border-danger border-5 bg-secondary-subtle w-25 m-auto" :"text-secondary bg-secondary-subtle w-25 m-auto"}
     key={i}>{date}</h3>
+
     )}
 </section>
   )
