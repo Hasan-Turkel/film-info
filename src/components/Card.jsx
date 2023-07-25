cef907bb77f387c9026ecb6627b16e8b7d1e2391
hasan <mmhotmail>
2023-07-25 22:03:48 +0300

    wrap commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,11 @@ const Card = ({data, index}) => {
     <article className="text-start mx-5">
             <h1 className="text-warning">{data?.[index].title}</h1>
             <p>{data?.[index].body}</p>
-            <h6 className="text-danger">{data?.[index].tags.join("/")}</h6>
+            <h6 className="text-danger ">{data?.[index].tags.join(" / ")}</h6>
             <div className="d-flex align-items-start gap-2">
             <StarRatingComponent className="fs-5"
               name={data?.[index].rate.toFixed(2)}
-              starCount={10}
+              starCount={5}
               value={Math.round(data?.[index].rate)}
               editing={true}
             />
