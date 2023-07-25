4f0107401758a308433867a1571938b5f63c5452
hasan <mmhotmail>
2023-07-25 21:19:44 +0300

    button commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,32 +1,32 @@
 import  {data}  from "../helper/data";
 import StarRatingComponent from "react-star-rating-component";
+import {useState } from "react";
 
 const Main = () => {
     // console.log(data);
+    const [index, setIndex] = useState(0)
 
   return (
     <main className="container my-5  p-5 shadow bg-body-tertiary rounded">
         <section className="d-flex gap-4 m-3">
-            {data.map(({date}, i)=><h3 role="button" className="text-secondary bg-secondary-subtle w-25 m-auto" key={i}>{date}</h3>
+            {data.map(({date}, i)=><h3 onClick={()=>setIndex(i)} role="button" className={i==index?"text-danger border-bottom border-danger border-5 bg-secondary-subtle w-25 m-auto" :"text-secondary bg-secondary-subtle w-25 m-auto"}
+            key={i}>{date}</h3>
             )}
 
         </section>
         <article className="text-start mx-5">
-            <h1 className="text-warning">{data?.[0].title}</h1>
-            <p>{data?.[0].body}</p>
-            <h6 className="text-danger">{data?.[0].tags.join("/")}</h6>
+            <h1 className="text-warning">{data?.[index].title}</h1>
+            <p>{data?.[index].body}</p>
+            <h6 className="text-danger">{data?.[index].tags.join("/")}</h6>
             <div className="d-flex align-items-start gap-2">
             <StarRatingComponent className="fs-5"
               name="data?.[0].rate"
               starCount={10}
-              value={Math.round(data?.[0].rate)}
-            //   editing={true}
+              value={Math.round(data?.[index].rate)}
+              editing={true}
             />
-
-            <h4 className="fs-4">{data?.[0].rate.toFixed(2)}/10</h4>
+            <h4 className="fs-4">{data?.[index].rate.toFixed(2)}/10</h4>
             </div>
-
-
         </article>
     </main>
     
