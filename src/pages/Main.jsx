b6b335c50b390fcc38371231b59b80fd483a2908
hasan <mmhotmail>
2023-07-25 17:30:38 +0300

    css commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,25 +3,27 @@ import StarRatingComponent from "react-star-rating-component";
 
 const Main = () => {
     // console.log(data);
+
   return (
-    <main>
-        <section>
-            {data.map(({date}, i)=><h3 key={i}>{date}</h3>
+    <main className="container my-5  p-5 shadow bg-body-tertiary rounded">
+        <section className="d-flex gap-4 m-3">
+            {data.map(({date}, i)=><h3 role="button" className="text-secondary bg-secondary-subtle w-25 m-auto" key={i}>{date}</h3>
             )}
+
         </section>
-        <article>
-            <h2>{data?.[0].title}</h2>
+        <article className="text-start mx-5">
+            <h1 className="text-warning">{data?.[0].title}</h1>
             <p>{data?.[0].body}</p>
-            <h4>{data?.[0].tags.join("/")}</h4>
-            <div>
-            <StarRatingComponent
+            <h6 className="text-danger">{data?.[0].tags.join("/")}</h6>
+            <div className="d-flex align-items-start gap-2">
+            <StarRatingComponent className="fs-5"
               name="data?.[0].rate"
-              starCount={5}
-              value={Math.round(data?.[0].rate / 2)}
+              starCount={10}
+              value={Math.round(data?.[0].rate)}
             //   editing={true}
             />
 
-            <h2>{data?.[0].rate.toFixed(2)} / 10</h2>
+            <h4 className="fs-4">{data?.[0].rate.toFixed(2)}/10</h4>
             </div>
 
 
